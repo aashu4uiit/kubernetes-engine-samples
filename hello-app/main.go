fa0213844a33b9be4c9fdeb17398bde03be957d6
aashu4uiit <aashu4uiit@yahoo.com>
2019-02-21 16:30:35 +1100

    Update main.go

--- hello-app/main.go
+++ hello-app/main.go
@@ -45,8 +45,9 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello, world!\n")
-	fmt.Fprintf(w, "Version: 1.0.0\n")
-	fmt.Fprintf(w, "Hostname: %s\n", host)
-}
+	fmt.Fprintf(w, "Hello Rafi,\n")
+	fmt.Fprintf(w, "This is my Kubernetes monitoring demo with Dynatrace platform,\n")
+	fmt.Fprintf(w, "Thanks\n")
+	fmt.Fprintf(w, "Aashish\n")
+	}
 // [END all]
